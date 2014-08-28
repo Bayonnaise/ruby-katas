fa0e58e35639f922020d348b1690caeefe569e9d
David Bayon <dbayon82@googlemail.com>
2014-08-28 16:42:40 +0100

    Refactored method

--- money_to_coins/money_to_coins.rb
+++ money_to_coins/money_to_coins.rb
@@ -1,20 +1,28 @@
+COINS = [
+	[200, '£2'],
+	[100, '£1'],
+	[50, '50p'],
+	[20, '20p'],
+	[10, '10p'],
+	[5, '5p'], 
+	[2, '2p'],
+	[1, '1p']
+]
+
 def money_to_coins(amount)
 	output = ""
 
-	if amount.chars.last == 'p'
-		amount = amount.gsub(/[p]/, '').to_i
-	elsif amount.chars.first == '£'
-		amount = (amount.gsub(/[£]/, '').to_f * 100).to_i
-	end
+	remaining = amount.gsub(/[p]/, '').to_i if amount.chars.last == 'p'
+	remaining = (amount.gsub(/[£]/, '').to_f * 100).to_i if amount.chars.first == '£'
 
-	[[200, '£2'], [100, '£1'], [50, '50p'], [20, '20p'], [10, '10p'], [5, '5p'], [2, '2p'], [1, '1p']].each do |coin, string|
-		next if amount < coin
+	COINS.each do |coin_value, coin_string|
+		next if remaining < coin_value
 
-		word = amount / coin
-		amount = amount % coin
+		coin_quantity = remaining / coin_value
+		remaining = remaining % coin_value
 
-		output.prepend("#{word}x#{string}") unless word == 0
-		output.prepend(" ") unless amount == 0
+		output.prepend("#{coin_quantity}x#{coin_string}") unless coin_quantity == 0
+		output.prepend(" ") unless remaining == 0
 	end 
 
 	output
